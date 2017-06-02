1be3be310f17ee993a0444503751bee23f7ea2b7
WIN_NT\x16j025 <x16j025@chiba-fjb.ac.jp>
2017-06-02 12:19:56 +0900

    キーボード処理

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(254, 277);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 337);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,85 @@ namespace game
 {
     public partial class Form1 : Form
     {
+        int speed = 10;
+        int moveX;
+        int moveY;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveY = -speed;
+            moveX = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            moveY = +speed;
+            moveX = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moveX = -speed;
+            moveY = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            moveX = +speed;
+            moveY = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += moveX;
+            pictureBox1.Top += moveY;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                moveY = -speed;
+                moveX = 0;
+            }else if(e.KeyCode == Keys.Left)
+            {
+                moveX = -speed;
+                moveY = 0;
+            }
+            else if(e.KeyCode == Keys.Right)
+            {
+                moveX = +speed;
+                moveY = 0;
+            }
+            else if(e.KeyCode == Keys.Down)
+            {
+                moveY = +speed;
+                moveX = 0;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                moveY = 0;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                moveX = 0;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                moveX = 0;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                moveY = 0;
+            }
         }
     }
 }
